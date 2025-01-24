121e433f4f7c7cc084dd4ee17b67a099255794d7
Clayton Roberto Dos Santos <clayton.santos.s035@gmail.com>
2025-01-23 22:56:00 -0300

    adicionando-rotas-das-views

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
+const path = require('path');
 const db = require("./db/connection");
 const bodyParser = require('body-parser');
 
@@ -11,6 +13,12 @@ app.listen(port, function(){
 
 // body Parser
 app.use(bodyParser.urlencoded({extended: false}));
+
+// handle bars
+//diretório base da aplicação.
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('views engine', 'handlebars');
 
 // DB connection
 db.authenticate()
